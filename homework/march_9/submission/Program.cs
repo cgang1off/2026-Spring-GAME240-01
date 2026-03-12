28cf9451d1e73d4135a0640c34cd5f62439a2b4b
cgang1off <gangloff@chapman.edu>
2026-03-12 16:30:39 -0700

    hw 03-09

---
+++ homework/march_9/submission/Program.cs
@@ -0,0 +1,22 @@
+﻿int [] numbers = [1, 2, 3, 4, 5];
+
+int min = numbers[0];
+int max = numbers[0];
+int i = 0;
+
+while (i < numbers.Length)
+{
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+
+    i++;
+}
+ Console.WriteLine("The smallest number is: " + min);
+ Console.WriteLine("The largest number is: " + max);
