7e612030d830e8e1e3cff704094011e5e2b2e0fb
cgang1off <gangloff@chapman.edu>
2026-03-31 15:55:04 -0700

    April 2 homework

---
+++ homework/april_2/Program.cs
@@ -0,0 +1,52 @@
+﻿
+double a, b;
+char operation; 
+
+Console.WriteLine("Enter your equation (ex. 12+34):");
+string input =  Console.ReadLine();
+
+int opIndex = input.IndexOfAny(['+', '-', '/', '*', '%']);
+
+if (opIndex == -1) 
+{
+    Console.WriteLine("Invalid input format.");
+return;
+}
+
+
+a = double.Parse(input.Substring(0, opIndex));
+
+operation = input[opIndex];
+
+b = double.Parse(input.Substring(opIndex + 1));
+
+double result = 0;
+
+switch (operation) 
+{
+    case '+':
+        result = a + b;
+        break;
+    case '-':
+        result = a - b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    case '/':
+        if (b == 0)
+        {
+            Console.WriteLine("You can't divide by zero.");
+            return; 
+        }
+        result = a / b;
+        break;
+    case '%':
+        result = a % b;
+        break;
+    default:
+        Console.WriteLine("Invalid operation.");
+        return;
+}
+
+Console.WriteLine("Result: " + result);
